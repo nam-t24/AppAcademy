6473e56cfd693542c2e0c4d1ae1ecc0e960241e4
nam-t24 <namtruong831@gmail.com>
2021-06-19 14:30:39 -0500

    added LRU Cache

---
+++ LRU Cache/LRUCache.rb
@@ -0,0 +1,42 @@
+class LRUCache
+    attr_reader :size
+    def initialize(size)
+        @cache =[]
+        @size=size
+    end
+
+    def count
+        @cache.size
+    end
+
+    def add(el)
+        @cache.delete(el) if @cache.include?(el)
+        @cache.shift if self.count >= size
+        @cache << el
+    end
+
+    def show
+        p @cache
+    end
+
+    private
+
+end
+
+johnny_cache = LRUCache.new(4)
+
+johnny_cache.add("I walk the line")
+johnny_cache.add(5)
+
+p johnny_cache.count # => returns 2
+
+johnny_cache.add([1,2,3])
+johnny_cache.add(5)
+johnny_cache.add(-5)
+johnny_cache.add({a: 1, b: 2, c: 3})
+johnny_cache.add([1,2,3,4])
+johnny_cache.add("I walk the line")
+johnny_cache.add(:ring_of_fire)
+johnny_cache.add("I walk the line")
+johnny_cache.add({a: 1, b: 2, c: 3})
+johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
